92a94ef56ff7d7361ca9a907d5313ba3930f4c3c
Nico Cousiño <nicocorpaorations@gmail.com>
2021-10-18 19:38:09 -0300

    Chocola V.2
    Cambia al apretar botones,  Lista de computadoras vacia

--- frontend/src/componentes/encuesta.js
+++ frontend/src/componentes/encuesta.js
@@ -1,113 +1,141 @@
 import React, { useState } from "react";
 import Chocola from "./Chocola.png";
+import ChocolaHappy from "./ChocolaHappy.png";
+import ComputadoraDataService from "../servicios/computadora";
 
 
 
 
-const Encuesta = props => {
-//useState(parametro) ->Le aplica la funcion(setter) a la constante y setea el valor inicial que es el parametro
-  const [nroPregunta, updatePregunta] = useState(0);
+class Encuesta extends React.Component{
 
-  var preguntas=[
-    "¿Que uso vas a darle a tu computadora?",
-    "¿La usas para trabajar?"
-];
-var respuestas=[];
-  //document.getElementById("preguntaHeader").innerHTML=preguntas[nroPregunta];
+  constructor(props){
+    super(props);
 
-  function actualizarPregunta(valor,nroPregunta){
-    updatePregunta(nroPregunta+1)
-    respuestas[nroPregunta]=valor;
-    console.log(respuestas[nroPregunta] + "Este es el resultado");
+    this.state = {nroPregunta: 0,
+      preguntas:[
+        "¿Que uso vas a darle a tu computadora?",
+        "¿La usas para trabajar?",
+        "¿Jugas a los jueguitos?"
+    ], respuestas:[], computadoras: []
+  };
+
+
+    
+    this.actualizarPregunta = this.actualizarPregunta.bind(this);
+    this.incPregunta = this.incPregunta.bind(this);
+    this.getRadioSeleccionado = this.getRadioSeleccionado.bind(this);
+    this.incPregunta = this.incPregunta.bind(this);
+    this.traerComputadora = this.traerComputadora.bind(this);
+    
+    
   }
 
-  function guardarRespuesta(){}
+//useState(parametro) ->Le aplica la funcion(setter) a la constante y setea el valor inicial que es el parametro
+  
+//const [nroPregunta, updatePregunta] = useState(0);
+  //document.getElementById("preguntaHeader").innerHTML=preguntas[nroPregunta];
 
-  function getRadioSeleccionado (){
-    console.log("Entro a la fuincion");
+  actualizarPregunta(valor){
+    this.state.respuestas[this.state.nroPregunta]=valor;
+    console.log(this.state.respuestas[this.state.nroPregunta] + " Este es el resultado");
+    this.incPregunta();
+  }
+
+  incPregunta(){
+    this.setState({nroPregunta: this.state.nroPregunta + 1})
+
+  }
+
+  getRadioSeleccionado (){
 
     var lista = document.getElementsByName("Uso")
     var i =0;
     var encontrado= false;
-    console.log(lista.length);
     var result;
     while (i<lista.length  && !encontrado){
-      console.log("Entro al while");
       if (lista[i].checked){
-        console.log("Entro al if");
         encontrado = true;
         result = lista[i].value;
       }
       i++;
     }
-    actualizarPregunta(result, nroPregunta);
+    this.actualizarPregunta(result);
+    this.incPregunta();
+
+  }
+  traerComputadora(){ //Falta bind
+    ComputadoraDataService.find(this.state.respuestas[1],"brand")
+    .then(
+      (response)=> this.setState({computadoras: response.data.computadoras})
+    );
+
   }
 
-  
-
-  
-    return (
-
-
-      <div className="App-sayname">
-
-        <div>
-          <h2>Pregunta</h2>
-          <br/>
-          <h2 id="preguntaHeader">{preguntas[nroPregunta]}</h2>
-          <div>
-          Programacion
-          <input type="radio"name="Uso" value="Programacion"/>
-          Jugar
-          <input type="radio"name="Uso" value = "Jugar"/>
-          Diseño
-          <input type="radio"name="Uso" value="Diseño"/>
-          <button
-              className="Boton de radio"
-              type="button"
-              onClick={getRadioSeleccionado}
-            >Enviar </button>
-          </div>
-
-          
-          <img src={Chocola} height="" alt="Its getting bigger!" />
-
-
-
-        </div>
+  render(){
+    /* La estructura divina*/
+    if (this.state.nroPregunta==0){
+      return (
         
-        <h2 className="input-helper">
-        Choices para encontrar tu computadora ideal
-        </h2>
-
-       <div className="card">
-            <div className="card-body">
-            <h5 className="card-title">DISEÑO </h5>
-            <p className="card-text">
-            </p>
-            <div className="row">
-            
-              <a target="_blank" href={"https://www.google.com/maps/place/" } className="btn btn-primary col-lg-5 mx-1 mb-1">
-                  Ver Computadoras</a>
+        <div className="App-sayname">
+  
+          <div>
+            <br/>
+            <h2 id="preguntaHeader">{this.state.preguntas[this.state.nroPregunta]}</h2>
+            <div>
+            Programacion
+            <input type="radio"name="Uso" value="Programacion"/>
+            Jugar
+            <input type="radio"name="Uso" value = "Jugar"/>
+            Diseño
+            <input type="radio"name="Uso" value="Diseño"/>
+            <button
+                className="Boton de radio"
+                type="button"
+                onClick={() => this.getRadioSeleccionado()}
+              >Enviar </button>
             </div>
+            <img src={Chocola} height="" alt="Its getting bigger!" />
+  
           </div>
-
-
-          <div className="card-body">
-            <h5 className="card-title">PROGRAMACION </h5>
-            <p className="card-text">
-            </p>
-            <div className="row">
-            
-              <a target="_blank" href={"https://www.google.com/maps/place/" } className="btn btn-primary col-lg-5 mx-1 mb-1">
-                  Ver Computadoras</a>
+          </div>
+      );
+    }
+    else if  (this.state.nroPregunta==1){
+      return (
+        <div className="App-sayname">
+          <div>
+            <h2>Pregunta</h2>
+            <br/>
+            <h2 id="preguntaHeader">{this.state.preguntas[this.state.nroPregunta]}</h2>
+            <div>
+            <button
+                className="Boton de radio"
+                type="button"
+                onClick={() => this.actualizarPregunta("Lenovo")}
+              >Lenovo </button>
+              <button
+                className="Boton de radio"
+                type="button"
+                onClick={() => this.actualizarPregunta("NoTrabajar")}
+              >No capo </button>
             </div>
+            <img src={ChocolaHappy} height="" alt="Its getting bigger!" />
+  
           </div>
-
-
           </div>
-      </div>
-    )
+      );
+    }
+    else {
+
+        return (
+
+          <div>{console.log(this.state.computadoras)}
+          {console.log(this.state.respuestas[1])}
+          </div>
+        );
+    }
   }
+}
+
   
   export default Encuesta;
